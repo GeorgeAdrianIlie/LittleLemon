fc4a9885ea577250b461df83e02174f8585d0637
GeorgeAdrianIlie <ilie.george.adrian@gmail.com>
2024-01-02 16:18:18 +0200

    C3

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Menu
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+def menu(request):
+    menu_data = Menu.objetcs.all()
+    main_data = {'menu':menu_data}
+    return render(request, 'menu.html', {'menu':main_data})
